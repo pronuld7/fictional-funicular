e84e5f264b145e9e960b3805240adf45478db8e6
pronuld7 <Pron_uld7@student.itstep.org>
2023-11-10 12:38:56 -0600

    Add main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import time
+
+# get the start time
+st = time.time()
+
+# main program
+# find sum to first 1 million numbers
+print("Hello")
+time.sleep(1)
+print("World!")
+
+# get the end time
+et = time.time()
+
+# get the execution time
+elapsed_time = et - st
+print('Execution time:', elapsed_time, 'seconds')
